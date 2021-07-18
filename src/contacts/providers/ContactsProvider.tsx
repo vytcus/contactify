c457040e3a1a058d44b7b531eb46f8beaa59d2b9
Vytautas Gerulis <vgerulis@alnasoft.lt>
2021-07-18 22:02:01 +0300

    Add useMount

--- src/contacts/providers/ContactsProvider.tsx
+++ src/contacts/providers/ContactsProvider.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useEffect } from 'react';
+import { createContext, ReactNode } from 'react';
 import { getContacts } from '../../common/api/requests';
 import useApi from '../../common/hooks/useApi';
+import useMount from '../../common/hooks/useMount';
 import { Contact, ContactsContext as ContactsContextType } from '../model';
 
 const initialContext: ContactsContextType = { contacts: [], loading: false };
@@ -14,9 +15,7 @@ interface Props {
 function ContactsProvider({ children }: Props) {
   const { loading, data, call } = useApi<Contact[]>();
 
-  useEffect(() => {
-    call(getContacts());
-  }, []);
+  useMount(() => call(getContacts()));
 
   return <ContactsContext.Provider value={{ loading, contacts: data ?? [] }}>{children}</ContactsContext.Provider>;
 }
