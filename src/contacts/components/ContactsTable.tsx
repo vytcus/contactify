5f2f24d9cc8bd6c6f1a8c920724c899dab8e9679
Vytautas Gerulis <vgerulis@alnasoft.lt>
2021-07-20 20:38:37 +0300

    Add back TableContainer | Add Filter button

--- src/contacts/components/ContactsTable.tsx
+++ src/contacts/components/ContactsTable.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from 'react';
-import { Table, TableHead, TableRow, TableCell, TableBody, Typography, Box, makeStyles } from '@material-ui/core';
+import { Table, TableHead, TableRow, TableCell, TableBody, Typography, Box, makeStyles, TableContainer } from '@material-ui/core';
 import { ContactsContext } from '../providers/ContactsProvider';
 import HeaderCell from './HeaderCell';
 import ContactCard from './ContactCard';
@@ -13,35 +13,37 @@ function ContactsTable() {
 
   return (
     <Box display="flex">
-      <Table size="medium">
-        <TableHead>
-          <TableRow>
-            <HeaderCell text="Name" />
-            <HeaderCell text="City" />
-            <HeaderCell text="Email" />
-            <HeaderCell text="Phone" align="right" />
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {contacts.map((x) => (
-            <TableRow key={x.id}>
-              <TableCell>
-                <Typography>{x.name}</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>{x.city}</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>{x.email}</Typography>
-              </TableCell>
-              <TableCell align="right">
-                <Typography>{x.phone}</Typography>
-              </TableCell>
+      <TableContainer>
+        <Table size="medium">
+          <TableHead>
+            <TableRow>
+              <HeaderCell text="Name" />
+              <HeaderCell text="City" />
+              <HeaderCell text="Email" />
+              <HeaderCell text="Phone" align="right" />
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+
+          <TableBody>
+            {contacts.map((x) => (
+              <TableRow key={x.id}>
+                <TableCell>
+                  <Typography>{x.name}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{x.city}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{x.email}</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography>{x.phone}</Typography>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
 
       {selectedId && <ContactCard contactId={selectedId} />}
     </Box>
--- src/contacts/components/ContactsTableFilters.tsx
+++ src/contacts/components/ContactsTableFilters.tsx
@@ -1,18 +1,23 @@
-import { Box, Checkbox, FormControl, FormControlLabel, InputLabel, makeStyles, Select, TextField, Typography } from '@material-ui/core';
+import { Box, Button, Checkbox, FormControl, FormControlLabel, InputLabel, makeStyles, Select, TextField, Typography } from '@material-ui/core';
 import blue from '@material-ui/core/colors/blue';
 
 const useStyles = makeStyles(() => ({
   formControl: {
     minWidth: 120,
   },
+  btn: {
+    width: 100,
+    borderRadius: 15,
+    color: 'white',
+  },
 }));
 
 function ContactsTableFilters() {
-  const { formControl } = useStyles();
+  const { formControl, btn } = useStyles();
 
   return (
     <Box display="flex" bgcolor={blue[700]}>
-      <Box display="flex" flex={1} bgcolor={blue[800]} borderRadius="10px 10px 0px 0px">
+      <Box display="flex" flex={1} alignItems="center" bgcolor={blue[800]} borderRadius="10px 10px 0px 0px" px={2}>
         <TextField label="Name" className={formControl} />
         <FormControl className={formControl}>
           <InputLabel id="contacts-table-city-filter-label">City</InputLabel>
@@ -21,6 +26,9 @@ function ContactsTableFilters() {
         <FormControl className={formControl}>
           <FormControlLabel control={<Checkbox color="primary" />} label="Show active" />
         </FormControl>
+        <Button variant="contained" color="primary" className={btn}>
+          Filter
+        </Button>
       </Box>
       <Box py={2} px={4}>
         <Typography color="textSecondary" variant="h3" component="h1">
