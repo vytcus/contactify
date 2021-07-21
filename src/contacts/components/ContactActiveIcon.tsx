3ecc96b7dcd349656b4e4d953ba4deb0e5af0fd0
Vytautas Gerulis <vgerulis@alnasoft.lt>
2021-07-21 15:20:21 +0300

    Change icon color

--- src/contacts/components/ContactActiveIcon.tsx
+++ src/contacts/components/ContactActiveIcon.tsx
@@ -1,16 +1,26 @@
+import { makeStyles } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+
+const useStyles = makeStyles(() => ({
+  icon: {
+    color: grey[600],
+  },
+}));
 
 interface Props {
   active: boolean;
 }
 
 function ContactActiveIcon({ active }: Props) {
+  const { icon } = useStyles();
+
   if (active) {
-    return <VisibilityIcon />;
+    return <VisibilityIcon className={icon} />;
   }
 
-  return <VisibilityOffIcon />;
+  return <VisibilityOffIcon className={icon} />;
 }
 
 export default ContactActiveIcon;
